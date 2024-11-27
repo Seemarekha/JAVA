fd62a9f4b8bbc1e9fb37204563622243df654f57
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-11-27 23:54:22 +0530

    Update A.java

--- Oops/src/com/inheritance/Single_Level/A.java
+++ Oops/src/com/inheritance/Single_Level/A.java
@@ -1,7 +1,20 @@
 package com.inheritance.Single_Level;
+
 class B{
+	void m1()
+	{
+		System.out.println("M1 method is called");
+	}
 	
 }
 public class A extends B {
-
+	void m2()
+	{
+		System.out.println("M2 method is called");
+	}
+	public static void main(String[] args) {
+		A a=new A();
+		a.m1();
+		a.m2();
+	}
 }
