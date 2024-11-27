baafa63d682ff0da36663ff6f2025daf32c9e606
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-11-27 23:54:08 +0530

    Update Test.java

--- Oops/src/com/inheritance/Multi_Level/Test.java
+++ Oops/src/com/inheritance/Multi_Level/Test.java
@@ -1,5 +1,38 @@
 package com.inheritance.Multi_Level;
 
+class Animal{
+	void sound()
+	{
+		System.out.println("sounds...");
+	}
+}
+class Dog extends Animal{
+
+	@Override
+	void sound() {
+		System.out.println("Barking");
+	}
+	
+	
+}
+class BabyDog extends Dog{
+	void drinkMilk()
+	{
+		System.out.println("Drinking milk");
+	}
+	
+}
 public class Test {
+	public static void main(String[] args) {
+		Animal a=new Animal();
+		a.sound();
+		
+		Dog d=new Dog();
+		d.sound();
+		
+		BabyDog b=new BabyDog();
+		b.sound();
+		b.drinkMilk();
+	}
 
 }
