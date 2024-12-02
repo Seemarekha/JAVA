5d8144dd07725d6456ec4354d9d3ad0962e23199
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-02 14:54:18 +0530

    Create PrintWordsofGivenString.java

---
+++ String_Programs/src/com/string_programs/PrintWordsofGivenString.java
@@ -0,0 +1,32 @@
+package com.string_programs;
+
+import java.util.Scanner;
+
+public class PrintWordsofGivenString {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter a string : ");
+		String s = scn.nextLine();
+		String s2=" ";
+		String[] str=s.split(" ");
+		
+		for(int i=0;i<str.length;i++)
+		{
+			System.out.println(str[i]);
+		}
+
+	}
+
+}
+
+
+/*
+
+i/p : java is very easy
+o/p : java
+      is
+      very
+      easy
+
+*/
