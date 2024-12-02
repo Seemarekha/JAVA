49534f4a6432af6d6c300197b3c145f13559df2a
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-02 14:53:54 +0530

    Create WordOfStringInReverse.java

---
+++ String_Programs/src/com/string_programs/WordOfStringInReverse.java
@@ -0,0 +1,38 @@
+package com.string_programs;
+
+import java.util.Scanner;
+
+public class WordOfStringInReverse {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter a string : ");
+		String s=scn.nextLine();
+		String[] str=s.split(" ");
+		String s2="";
+		
+		for(int i=0;i<str.length;i++)
+		{
+			String word=str[i];
+			String revWord="";
+			for(int j=word.length()-1;j>=0;j--)
+			{
+				revWord+=word.charAt(j);
+			}
+			s2+=revWord;
+			if(i<str.length-1)
+				s2+=" ";
+		}
+
+		System.out.println("Reverser string : "+s2);
+	}
+
+}
+
+
+/*
+
+i/p : java is very easy
+o/p : avaj si yrev ysae
+
+*/
