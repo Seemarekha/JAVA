6a1ef71a59a9aa8a50124b54f4eb5b22937a4133
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-02 14:54:06 +0530

    Create Sutun_Stn.java

---
+++ String_Programs/src/com/string_programs/Sutun_Stn.java
@@ -0,0 +1,15 @@
+package com.string_programs;
+
+import java.util.Scanner;
+
+public class Sutun_Stn {
+
+	public static void main(String[] args) {
+		String ip="Sutun";
+		String op=ip.replace("u", "");
+		System.out.println(op);
+		
+
+	}
+
+}
