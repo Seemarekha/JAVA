c86589c83544f48007d5cdab2c7f4797d3520ecd
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-04 22:54:50 +0530

    Create SwappingTwoString.java

---
+++ String_Programs/src/com/string_programs/SwappingTwoString.java
@@ -0,0 +1,26 @@
+// write a java program to swap two string variables without using third variable? 
+
+
+
+package com.string_programs;
+
+public class SwappingTwoString {
+
+	public static void main(String[] args) {
+		String s1="Hello";
+		String s2="World";
+		
+		System.out.println("Before swapping ...");
+		System.out.println(s1+" "+s2);
+		
+		System.out.println("After swapping ...");
+		
+		s1=s1+s2;
+		s2=s1.substring(0, s1.length()-s2.length());
+		s1=s1.substring(s2.length());
+		
+		System.out.println(s1+" "+s2);
+
+	}
+
+}
