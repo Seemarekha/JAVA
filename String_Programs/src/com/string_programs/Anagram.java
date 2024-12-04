748c144184927b85109a44ad831a36fb1350c901
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-04 23:47:32 +0530

    Update Anagram.java

--- String_Programs/src/com/string_programs/Anagram.java
+++ String_Programs/src/com/string_programs/Anagram.java
@@ -1,5 +1,6 @@
 package com.string_programs;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Anagram {
@@ -14,10 +15,19 @@ public class Anagram {
 			System.out.println("Its Anagram");
 		else
 			System.out.println("Its Not Anagram");
+		
+		/***************************/
+		
+		if(Anagram(s1,s2))
+			System.out.println("Its Anagram");
+		else
+			System.out.println("Its Not Anagram");
 
 	}
+	
+	/*************Approach-1***************/
 
-	 static boolean isAnagram(String s1, String s2) {
+	static boolean isAnagram(String s1, String s2) {
 		while(true)
 		{
 			if(s1.length()!=s2.length())
@@ -29,5 +39,19 @@ public class Anagram {
 			s2=s2.replace(c+"", "");
 		}
 	}
+	 
+	 /*************Approach-2***************/
+	
+	static boolean Anagram(String s1, String s2)
+	{
+		char[] str1=s1.toCharArray();
+		char[] str2=s2.toCharArray();
+		
+		Arrays.sort(str1);
+		Arrays.sort(str2);
+		return Arrays.equals(str1, str2);
+	}
+	 
+
 
 }
