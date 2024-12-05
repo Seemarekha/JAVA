60ab962426f6ac2cb82bc01df897c7a95b70ea9e
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-05 21:41:57 +0530

    Create RemoveDuplicatesUsingIndexOf.java

---
+++ String_Programs/src/com/string_programs/RemoveDuplicatesUsingIndexOf.java
@@ -0,0 +1,26 @@
+//  input: “ramana” output:”ramn” with indexOf method?
+
+package com.string_programs;
+
+import java.util.Scanner;
+
+public class RemoveDuplicatesUsingIndexOf {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a string : ");
+		String s=new Scanner(System.in).next();
+		String result="";
+		for(int i=0;i<s.length();i++)
+		{
+			char c=s.charAt(i);
+			if(result.indexOf(c)==-1)
+			{
+				result+=c;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+}
+ 
