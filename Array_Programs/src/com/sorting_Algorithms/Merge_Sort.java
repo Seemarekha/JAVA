c9aea47467abcf3e4fc4e066547f9914193373fa
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-08 14:29:03 +0530

    Create Merge_Sort.java

---
+++ Array_Programs/src/com/sorting_Algorithms/Merge_Sort.java
@@ -0,0 +1,52 @@
+package com.sorting_Algorithms;
+
+import java.util.Arrays;
+
+public class Merge_Sort {
+
+	public static void main(String[] args) {
+
+		int[] a = { 5, 8, 1, 2, 4, 6, 8, 10, 5, 9 };
+		System.out.println("Before sorting : " + Arrays.toString(a));
+
+		sort(a);
+		System.out.println("After sorting : " + Arrays.toString(a));
+
+	}
+
+	static void sort(int[] a) {
+
+		if (a.length == 1)
+			return;
+
+		int[] left = new int[a.length / 2];
+		int[] right = new int[a.length - left.length];
+
+		System.arraycopy(a, 0, left, 0, left.length);
+		System.arraycopy(a, left.length, right, 0, right.length);
+
+		sort(left);
+		sort(right);
+
+		merge(left, right, a);
+
+	}
+
+	static void merge(int[] a, int[] b, int[] c) {
+
+		int i = 0, j = 0, k = 0;
+
+		while (i < a.length && j < b.length) {
+			if (a[i] < b[j])
+				c[k++] = a[i++];
+			else
+				c[k++] = b[j++];
+		}
+
+		while (i < a.length)
+			c[k++] = a[i++];
+		while (j < b.length)
+			c[k++] = b[j++];
+	}
+
+}
