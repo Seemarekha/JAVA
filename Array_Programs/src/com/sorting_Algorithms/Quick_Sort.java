d2a1337b3fb2f9599f6377418e3bdbd4ec9e6240
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-08 14:29:08 +0530

    Create Quick_Sort.java

---
+++ Array_Programs/src/com/sorting_Algorithms/Quick_Sort.java
@@ -0,0 +1,48 @@
+package com.sorting_Algorithms;
+
+import java.util.Arrays;
+
+public class Quick_Sort {
+
+	public static void main(String[] args) {
+
+		int[] a = { 5, 3, 8, 1, 2, 4, 6, 7, 10, 5, 9 };
+		System.out.println("Before sorting : " + Arrays.toString(a));
+
+		sort(a, 0, a.length - 1);
+		System.out.println("After sorting : " + Arrays.toString(a));
+
+	}
+
+	static void sort(int[] a, int start, int end) {
+
+		if (start >= end)
+			return;
+
+		int i = start;
+		int j = end;
+
+		int pivot = a[(start + end) / 2];
+
+		while (i <= j) {
+			while (a[i] < pivot)
+				i++;
+			while (a[j] > pivot)
+				j--;
+
+			if (i <= j) {
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+
+				i++;
+				j--;
+			}
+		}
+
+		sort(a, start, j);
+		sort(a, i, end);
+
+	}
+
+}
