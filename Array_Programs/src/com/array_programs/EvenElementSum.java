a12f2b9968b254ed090a3ae422a3c19f698416df
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-09 16:40:20 +0530

    Create EvenElementSum.java

---
+++ Array_Programs/src/com/array_programs/EvenElementSum.java
@@ -0,0 +1,17 @@
+package com.array_programs;
+
+public class EvenElementSum {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int sum = 0;
+		for (int num : arr) {
+			if (num % 2 == 0) {
+				sum += num;
+			}
+		}
+		System.out.println("Sum of Even elements in the array:" + sum);
+
+	}
+
+}
