be1436ee65447455e9dada7ce3330d204946fcfb
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-09 16:40:34 +0530

    Create PrintEvenElementPresentInArray.java

---
+++ Array_Programs/src/com/array_programs/PrintEvenElementPresentInArray.java
@@ -0,0 +1,22 @@
+package com.array_programs;
+
+public class PrintEvenElementPresentInArray {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 10, 15, 22, 33, 40, 55, 60, 75, 80 };
+
+		System.out.println("Array values:");
+		for (int num : arr) {
+			System.out.print(num + " ");
+		}
+
+		System.out.println("\nEven elements in the array:");
+		for (int num : arr) {
+			if (num % 2 == 0) {
+				System.out.print(num + " ");
+			}
+		}
+	}
+
+}
