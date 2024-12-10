28c8a3911053defbd398433ad75d73a7a9b3d325
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-10 21:10:09 +0530

    Create InsertEvenValuesIntoArray.java

---
+++ Array_Programs/src/com/array_programs/InsertEvenValuesIntoArray.java
@@ -0,0 +1,25 @@
+package com.array_programs;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InsertEvenValuesIntoArray {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		ArrayList<Integer> evenValues = new ArrayList<Integer>();
+		System.out.print("Enter number of element u want to insert: ");
+		int n = scn.nextInt();
+		System.out.println("Enter values: ");
+		for (int i = 0; i < n; i++) {
+			int value = scn.nextInt();
+			if (value % 2 == 0)
+				evenValues.add(value);
+		}
+		System.out.println("Even values in array: ");
+		for (int evenValue : evenValues)
+			System.out.print(evenValue + " ");
+
+	}
+
+}
