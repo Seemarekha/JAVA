95c1a6082e31f601d6c8509cd2b9f034df0d70ee
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-11 21:03:26 +0530

    Create SumOfTwoArrays.java

---
+++ Array_Programs/src/com/array_programs/SumOfTwoArrays.java
@@ -0,0 +1,30 @@
+package com.array_programs;
+
+import java.util.Arrays;
+
+public class SumOfTwoArrays {
+
+	public static void main(String[] args) {
+		int[] a1 = { 2, 5 };
+		System.out.println("Array-1 : " + Arrays.toString(a1));
+		int[] a2 = { 6, 78, 34 };
+		System.out.println("Array-2 : " + Arrays.toString(a2));
+
+		int[] sum = addArrays(a1, a2);
+		System.out.println("Sum of two arrays : " + Arrays.toString(sum));
+
+	}
+
+	private static int[] addArrays(int[] a1, int[] a2) {
+		int maxLen = Math.max(a1.length, a2.length);
+		int[] res = new int[maxLen];
+
+		for (int i = 0; i < maxLen; i++) {
+			int v1 = (i < a1.length ? a1[i] : 0);
+			int v2 = (i < a2.length ? a2[i] : 0);
+			res[i] = v1 + v2;
+		}
+		return res;
+	}
+
+}
