f72d9cc31371a3ed6ee8b90c241a69cfdbb69414
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-11 21:03:08 +0530

    Create RemoveElementsPresentMoreThanOnce.java

---
+++ Array_Programs/src/com/array_programs/RemoveElementsPresentMoreThanOnce.java
@@ -0,0 +1,35 @@
+package com.array_programs;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class RemoveElementsPresentMoreThanOnce {
+
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 1, 3, 2, 4 };
+		System.out.println(Arrays.toString(a));
+		int[] result = uniqueElement(a);
+		System.out.println(Arrays.toString(result));
+
+	}
+
+	private static int[] uniqueElement(int[] a) {
+		HashMap<Integer, Integer> mp = new HashMap<Integer, Integer>();
+		for (int num : a) {
+			mp.put(num, mp.getOrDefault(num, 0) + 1);
+		}
+		int uniqueCount = 0;
+		for (int count : mp.values()) {
+			if (count == 1)
+				uniqueCount++;
+		}
+		int[] res = new int[uniqueCount];
+		int i = 0;
+		for (int num : a) {
+			if (mp.get(num) == 1)
+				res[i++] = num;
+		}
+		return res;
+	}
+
+}
