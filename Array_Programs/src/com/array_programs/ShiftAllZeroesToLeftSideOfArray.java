0d343f6e476e40fe373cebc143e0766fc7ed7435
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-12 23:42:03 +0530

    Create ShiftAllZeroesToLeftSideOfArray.java

---
+++ Array_Programs/src/com/array_programs/ShiftAllZeroesToLeftSideOfArray.java
@@ -0,0 +1,24 @@
+package com.array_programs;
+
+import java.util.Arrays;
+
+public class ShiftAllZeroesToLeftSideOfArray {
+
+	public static void main(String[] args) {
+		int[] a = { 1, 0, 4, 3, 0, 6, 0, 0, 9, 8 };
+		System.out.println("Before shifting : " + Arrays.toString(a));
+		int[] b = new int[a.length];
+
+		int j = a.length - 1;
+
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] != 0) {
+				b[j] = a[i];
+				j--;
+			}
+		}
+		System.out.println("After shifting : " + Arrays.toString(b));
+
+	}
+
+}
