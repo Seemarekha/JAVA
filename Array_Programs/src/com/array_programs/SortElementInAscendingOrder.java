7fbe7633cfe376d40e342354aa5f36c80ff1153c
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-14 23:04:08 +0530

    Create SortElementInAscendingOrder.java

---
+++ Array_Programs/src/com/array_programs/SortElementInAscendingOrder.java
@@ -0,0 +1,16 @@
+package com.array_programs;
+
+import java.util.Arrays;
+
+public class SortElementInAscendingOrder {
+
+	public static void main(String[] args) {
+		int[] a= {34,23,56,46,85,90,87,54};
+		System.out.println("Before sorting : "+Arrays.toString(a));
+		Arrays.sort(a);
+		System.out.println("After sorting : "+Arrays.toString(a));
+		
+
+	}
+
+}
