aa0d1c36bc085edc66500117c18ce909ae1c0e0c
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-20 22:43:51 +0530

    Create SpyNumberInGivenRangeUsingRecursion.java

---
+++ Number_Programs/src/com/SpyNumber/SpyNumberInGivenRangeUsingRecursion.java
@@ -0,0 +1,39 @@
+package com.SpyNumber;
+
+import java.util.Scanner;
+
+public class SpyNumberInGivenRangeUsingRecursion {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter a range :");
+		Scanner scn = new Scanner(System.in);
+
+		int a = scn.nextInt();
+		int b = scn.nextInt();
+
+		System.out.println("Spy Numbers present in given range :");
+		range(a, b);
+	}
+
+	private static void range(int a, int b) {
+		if (a == b)
+			return;
+		if (isSpyNumber(a, 0, 1))
+			System.out.print(a + " ");
+		range(a + 1, b);
+	}
+
+	private static boolean isSpyNumber(int n, int sum, int product) {
+		if (n == 0) {
+			if (sum == product)
+				return true;
+			return false;
+		}
+		sum += n % 10;
+		product *= n % 10;
+		return isSpyNumber(n /= 10, sum, product);
+
+	}
+
+}
