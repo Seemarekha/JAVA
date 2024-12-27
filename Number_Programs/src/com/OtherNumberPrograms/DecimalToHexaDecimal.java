8c107546cfe17a38f01913fc3b78c4b93e95d1d8
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2024-12-27 22:17:05 +0530

    Create DecimalToHexaDecimal.java

---
+++ Number_Programs/src/com/OtherNumberPrograms/DecimalToHexaDecimal.java
@@ -0,0 +1,37 @@
+package com.OtherNumberPrograms;
+
+import java.util.Scanner;
+
+public class DecimalToHexaDecimal {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter a decimal number : ");
+		int dec = new Scanner(System.in).nextInt();
+		System.out.println("Decimal To HexaDecimal Conversion : ");
+		convert(dec);
+
+	}
+
+	private static void convert(int dec) {
+
+		char[] hexa = new char[100];
+
+		int i = 0;
+		while (dec > 0) {
+			int rem = dec % 16;
+
+			if (rem < 10)
+				hexa[i++] = (char) (rem + 48);
+			else
+				hexa[i++] = (char) (rem + 55);
+
+			dec /= 16;
+		}
+
+		for (int j = i - 1; j >= 0; j--)
+			System.out.print(hexa[j]);
+
+	}
+
+}
