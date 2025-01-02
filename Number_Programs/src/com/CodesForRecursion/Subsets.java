6f6dabf3ab9629d882704072f3a8ff24cf44d1f7
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-01-02 20:34:57 +0530

    Create Subsets.java

---
+++ Number_Programs/src/com/CodesForRecursion/Subsets.java
@@ -0,0 +1,41 @@
+package com.CodesForRecursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 3, 4 };
+		List<List<Integer>> result = findSubsets(nums);
+
+		System.out.println("Subsets : ");
+
+		for (List<Integer> subset : result) {
+			System.out.println(subset);
+		}
+
+	}
+
+	private static List<List<Integer>> findSubsets(int[] nums) {
+		List<List<Integer>> subsets = new ArrayList<>();
+		generateSubsets(0, nums, new ArrayList<>(), subsets);
+		return subsets;
+	}
+
+	private static void generateSubsets(int index, int[] nums, List<Integer> curr, List<List<Integer>> subsets) {
+		if (index == nums.length) {
+			subsets.add(new ArrayList<Integer>(curr));
+			return;
+		}
+		generateSubsets(index + 1, nums, curr, subsets);
+
+		curr.add(nums[index]);
+
+		generateSubsets(index + 1, nums, curr, subsets);
+
+		curr.remove(curr.size() - 1);
+
+	}
+
+}
