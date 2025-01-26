a627062fa58b4e17cfebae45024a3e02b9cb7e29
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-01-26 22:22:22 +0530

    Create EqualizeArray.java

---
+++ Array_Programs/src/com/array_programs/EqualizeArray.java
@@ -0,0 +1,50 @@
+package com.array_programs;
+
+import java.util.Scanner;
+
+public class EqualizeArray {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+
+		System.out.print("Enter the size of the array: ");
+		int n = scn.nextInt();
+
+		int[] arr = new int[n];
+		System.out.println("Enter the elements of the array:");
+		for (int i = 0; i < n; i++) {
+			arr[i] = scn.nextInt();
+		}
+
+		if (canMakeEqual(arr)) {
+			System.out.println("Yes, all numbers can be made equal.");
+		} else {
+			System.out.println("No, all numbers cannot be made equal.");
+		}
+
+	}
+
+	private static boolean canMakeEqual(int[] arr) {
+		int n = arr.length;
+		if (n < 2)
+			return true;
+
+		int base = arr[0];
+		int gcdValue = 0;
+
+		for (int i = 1; i < n; i++) {
+			gcdValue = gcd(gcdValue, Math.abs(arr[i] - base));
+		}
+
+		return gcdValue != 0;
+	}
+
+	private static int gcd(int a, int b) {
+		while (b != 0) {
+			int temp = b;
+			b = a % b;
+			a = temp;
+		}
+		return a;
+	}
+}
