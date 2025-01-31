2c47b673f8a66e624fa8ad02eb9dbfab35536d90
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-01-31 11:17:18 +0530

    Update StringToInteger.java

--- String_Programs/src/com/string_programs/StringToInteger.java
+++ String_Programs/src/com/string_programs/StringToInteger.java
@@ -6,22 +6,17 @@ public class StringToInteger {
 
 	public static void main(String[] args) {
 		System.out.println("Enter a string : ");
-		String s=new Scanner(System.in).next();
-		
-		/***********Approach-1*********/
-		
-		int num=0;
-		for(int i=0;i<s.length();i++)
-		{
-			int n=s.charAt(i)-'0';
-			num=num*10+n;
-		}
-		System.out.println("String to Integer conversion : "+num);
+		String s = new Scanner(System.in).next();
 
-		/***********Approach-2*********/
-		
-		int num1=Integer.parseInt(s);
-		System.out.println("String to Integer conversion : "+num1);
+		/*********** Approach-1 *********/
+
+		int num = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int n = s.charAt(i) - '0';
+			num = num * 10 + n;
+		}
+		System.out.println("String to Integer conversion : " + num);
+
 	}
 
 }
