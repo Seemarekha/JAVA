9218b9e73c8781295f0ba849ff0b37d0760c7828
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-02-01 11:33:48 +0530

    Create MaximumNumberOfNetworks.java

---
+++ String_Programs/src/com/string_programs/MaximumNumberOfNetworks.java
@@ -0,0 +1,40 @@
+package com.string_programs;
+
+public class MaximumNumberOfNetworks {
+
+	public static void main(String[] args) {
+		int[] speed = { 5, 6, 9, 12, 10, 13 };
+		int minComp = 2;
+		int speedThreshold = 12;
+		int countNetwork = maxNetworks(speed, minComp, speedThreshold);
+		System.out.println("Number of network : " + countNetwork);
+
+	}
+
+	private static int maxNetworks(int[] speed, int minComp, int speedThreshold) {
+		int n = speed.length;
+		int countNetwork = 0;
+		int i = 0;
+		while (i < n) {
+			int sum = 0;
+			int numComp = 0;
+			boolean isValidNetwork = false;
+
+			for (int j = i; j < n; j++) {
+				sum += speed[j];
+				numComp++;
+				if (numComp >= minComp && sum >= speedThreshold) {
+					countNetwork++;
+					isValidNetwork = true;
+					i = j + 1;
+					break;
+				}
+			}
+			if (!isValidNetwork)
+				i++;
+		}
+
+		return countNetwork;
+	}
+
+}
