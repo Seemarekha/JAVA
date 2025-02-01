6767850e81c70d4b6ab54b456adc3cf70e65ceae
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-02-01 11:33:44 +0530

    Create LexicograpgicallySmallestStringAfterModification.java

---
+++ String_Programs/src/com/string_programs/LexicograpgicallySmallestStringAfterModification.java
@@ -0,0 +1,29 @@
+package com.string_programs;
+
+public class LexicograpgicallySmallestStringAfterModification {
+
+	public static void main(String[] args) {
+		String str = "hackerrank";
+		String res = getSmallestString(str);
+		System.out.println("The lexicographically smallest string : " + res);
+
+	}
+
+	private static String getSmallestString(String str) {
+		char[] arr = str.toCharArray();
+		boolean modified = false;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 'a') {
+				if (modified)
+					break;
+				continue;
+			}
+			modified = true;
+			arr[i] = (char) (arr[i] - 1);
+		}
+		if (!modified)
+			arr[arr.length - 1] = (char) (arr[arr.length - 1] - 1);
+		return new String(arr);
+	}
+
+}
