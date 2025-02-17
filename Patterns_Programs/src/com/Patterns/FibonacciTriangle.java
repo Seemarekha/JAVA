f2ce7b2b2912178cd3a06f8103529d0d16595a3e
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-02-17 21:13:05 +0530

    Update FibonacciTriangle.java

--- Patterns_Programs/src/com/Patterns/FibonacciTriangle.java
+++ Patterns_Programs/src/com/Patterns/FibonacciTriangle.java
@@ -36,3 +36,20 @@ public class FibonacciTriangle {
 	}
 
 }
+
+
+/***************** Output ******************/
+
+/*
+
+
+Enter number of lines : 5
+
+0	
+1	1	
+2	3	5	
+8	13	21	34	
+55	89	144	233	377	
+
+
+*/
