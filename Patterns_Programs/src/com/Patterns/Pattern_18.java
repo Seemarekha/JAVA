a6ef8d756bb73959be181637ea7ce29dc8507b2d
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-02-21 18:04:28 +0530

    Create Pattern_18.java

---
+++ Patterns_Programs/src/com/Patterns/Pattern_18.java
@@ -0,0 +1,42 @@
+package com.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_18 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Enter number of lines : ");
+		int n = scn.nextInt();
+		int sp = 0;
+		int star = n;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= sp; j++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= star; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			sp++;
+			star -= 2;
+		}
+
+	}
+
+}
+
+
+/****************** Output *************/
+
+
+/*
+
+Enter number of lines : 5
+
+  * * * * * 
+    * * * 
+      * 
+
+*/
