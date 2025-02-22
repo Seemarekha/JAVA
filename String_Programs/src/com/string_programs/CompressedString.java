b27312870163101c85f369acffafec19d40e7aff
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-02-23 00:03:16 +0530

    Update CompressedString.java

--- String_Programs/src/com/string_programs/CompressedString.java
+++ String_Programs/src/com/string_programs/CompressedString.java
@@ -4,28 +4,51 @@ import java.util.Scanner;
 
 public class CompressedString {
 
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		System.out.println("Enter a string :");
-		String str = scn.next();
-		System.out.println("Before compression : " + str);
-		int arr[] = new int[255];
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = scn.next();
+        scn.close();  
 
-		for (int i = 0; i < str.length(); i += 2) {
-			arr[str.charAt(i)] += str.charAt(i + 1) - '0';
-		}
+        System.out.println("Before compression: " + str);
 
-		String ans = "";
-		char ch=32;
+        int[] arr = new int[255];
 
-		for (int i = 0; i < 255; i++) {
-			if (arr[i] != 0) {
-				ch=(char)i;
-				ans=ans+ch+arr[i];
-			}
-		}
-		System.out.println("After compression : " + ans);
+        for (int i = 0; i < str.length() - 1; i += 2) { 
+            char character = str.charAt(i);
+            char countChar = str.charAt(i + 1);
 
-	}
+            if (Character.isDigit(countChar)) { 
+                arr[character] += countChar - '0';
+            } else {
+                System.out.println("Invalid input format!");
+                return;
+            }
+        }
 
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < 255; i++) {
+            if (arr[i] != 0) {
+                ans.append((char) i).append(arr[i]);
+            }
+        }
+
+        System.out.println("After compression: " + ans.toString());
+    }
 }
+
+
+
+/********* Output *************/
+
+/*
+ 
+ 
+Enter a string:
+a3b4a5c6b2
+Before compression: a3b4a5c6b2
+After compression: a8b6c6
+
+
+ */
