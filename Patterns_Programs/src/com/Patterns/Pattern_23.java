800cc1d8dfad7a60f9725cc5849aea5860d9f529
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-02-22 20:40:49 +0530

    Create Pattern_23.java

---
+++ Patterns_Programs/src/com/Patterns/Pattern_23.java
@@ -0,0 +1,52 @@
+package com.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_23 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Enter number of lines : ");
+		int n = scn.nextInt();
+		int sp = n / 2;
+		int star = 1;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= sp; j++) {
+				System.out.print("   ");
+			}
+			for (int j = 1; j <= star; j++) {
+				if (j == 1 || j == star)
+					System.out.print(" * ");
+				else
+					System.out.print("   ");
+			}
+			System.out.println();
+			if (i <= n / 2) {
+				star++;
+				sp--;
+			} else {
+				star--;
+				sp++;
+			}
+		}
+
+	}
+
+}
+
+/***************** Output *****************/
+
+/*
+ 
+Enter number of lines : 5
+  
+ 
+       * 
+    *  * 
+ *     * 
+    *  * 
+       * 
+       
+       
+ */
