d89c74e08ff9a3caa76d1e33f22f443b8ba1e860
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-02-24 23:16:57 +0530

    Update ArrayIsSubsetOfAnotherArray.java

--- Array_Programs/src/com/array_programs/ArrayIsSubsetOfAnotherArray.java
+++ Array_Programs/src/com/array_programs/ArrayIsSubsetOfAnotherArray.java
@@ -2,7 +2,7 @@ package com.array_programs;
 
 import java.util.HashSet;
 
-public class ArrayIsSubsetOfAotherArray {
+public class ArrayIsSubsetOfAnotherArray {
 
 	public static void main(String[] args) {
 
