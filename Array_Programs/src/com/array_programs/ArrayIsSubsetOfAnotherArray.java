962e42d5ae63f29deade87e012a7ff0769b4ea7e
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-02-25 13:05:53 +0530

    Update ArrayIsSubsetOfAnotherArray.java

--- Array_Programs/src/com/array_programs/ArrayIsSubsetOfAnotherArray.java
+++ Array_Programs/src/com/array_programs/ArrayIsSubsetOfAnotherArray.java
@@ -9,16 +9,13 @@ public class ArrayIsSubsetOfAnotherArray {
 		int arr1[] = { 11, 10, 13, 21, 30, 70 };
 		int arr2[] = { 11, 30, 70, 10 };
 
-		int m = arr1.length;
-		int n = arr2.length;
-
-		if (isSubset(arr1, arr2, m, n))
+		if (isSubset(arr1, arr2))
 			System.out.println("arr2 is subset of arr1");
 		else
 			System.out.println("arr2 is not subset of arr1");
 	}
 
-	private static boolean isSubset(int[] arr1, int[] arr2, int m, int n) {
+	private static boolean isSubset(int[] arr1, int[] arr2) {
 
 		HashSet<Integer> hs = new HashSet<Integer>();
 
