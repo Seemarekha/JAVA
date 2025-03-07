91bf73cf55ea2d4c1c76b76cd2cc44979515f5cd
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-03-07 09:43:26 +0530

    Create Rhombus.java

---
+++ Patterns_Programs/src/com/Patterns/Rhombus.java
@@ -0,0 +1,40 @@
+package com.Patterns;
+
+import java.util.Scanner;
+
+public class Rhombus {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Enter length : ");
+		int n = scn.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++)
+				System.out.print("  ");
+			for (int j = 1; j <= n; j++)
+				System.out.print("* ");
+			System.out.println();
+		}
+	}
+
+}
+
+
+
+
+
+/****************** Output ****************/
+
+/*
+
+
+Enter length : 4
+
+* * * * 
+  * * * * 
+    * * * * 
+      * * * * 
+
+
+*/
