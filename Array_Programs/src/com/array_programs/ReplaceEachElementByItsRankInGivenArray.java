a5afb5f8e607519b827a8bfb8f8b334b2deee4ec
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-03-08 21:19:03 +0530

    Create ReplaceEachElementByItsRankInGivenArray.java

---
+++ Array_Programs/src/com/array_programs/ReplaceEachElementByItsRankInGivenArray.java
@@ -0,0 +1,40 @@
+package com.array_programs;
+
+import java.util.Arrays;
+
+public class ReplaceEachElementByItsRankInGivenArray {
+
+	public static void main(String[] args) {
+		int[] arr = { 100, 2, 70, 12, 90 };
+		resultArray(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+	private static void resultArray(int[] arr) {
+		int[] res = new int[arr.length];
+		System.arraycopy(arr, 0, res, 0, arr.length);
+		Arrays.sort(res);
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (res[j] == arr[i]) {
+					arr[i] = j + 1;
+					break;
+				}
+			}
+		}
+
+	}
+
+}
+
+
+/***************** Output ******************/
+
+/*
+
+[5, 1, 3, 2, 4]
+
+
+*/
