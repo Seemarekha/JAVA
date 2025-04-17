529f7f766fb1d362c3f2a82292949c90bba9ce75
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-04-17 22:36:24 +0530

    Update SortFirstHalfInAscendingSecondHalfInDescending.java

--- Array_Programs/src/com/array_programs/SortFirstHalfInAscendingSecondHalfInDescending.java
+++ Array_Programs/src/com/array_programs/SortFirstHalfInAscendingSecondHalfInDescending.java
@@ -16,25 +16,42 @@ public class SortFirstHalfInAscendingSecondHalfInDescending {
         int mid = arr.length / 2;
 
        
-        int[] firstHalf = Arrays.copyOfRange(arr, 0, mid);
+//        int[] firstHalf = Arrays.copyOfRange(arr, 0, mid);
+//        
+//        Integer[] secondHalf = Arrays.stream(Arrays.copyOfRange(arr, mid, arr.length))
+//                                     .boxed()
+//                                     .toArray(Integer[]::new);
+//
+//        Arrays.sort(firstHalf);
+//
+//       
+//        Arrays.sort(secondHalf, Collections.reverseOrder());
+//
+//    
+//        int[] res = new int[arr.length];
+//        
+//        System.arraycopy(firstHalf, 0, res, 0, firstHalf.length);
+//        
+//        for (int i = mid; i < arr.length; i++) {
+//            res[i] = secondHalf[i - mid];
+//        }
         
-        Integer[] secondHalf = Arrays.stream(Arrays.copyOfRange(arr, mid, arr.length))
-                                     .boxed()
-                                     .toArray(Integer[]::new);
-
+        int[] firstHalf=new int[mid];
+        int[] secondHalf=new int[arr.length-mid];
+        
+        System.arraycopy(arr, 0, firstHalf, 0, mid);
+        System.arraycopy(arr, mid, secondHalf, 0, secondHalf.length);
+        
         Arrays.sort(firstHalf);
-
-       
-        Arrays.sort(secondHalf, Collections.reverseOrder());
-
-    
-        int[] res = new int[arr.length];
+        Arrays.sort(secondHalf);
+        int[] secHalf=new int[secondHalf.length];
+        int j=0;
+        for(int i=secondHalf.length-1;i>=0;i--)
+        	secHalf[j++]=secondHalf[i];
         
-        System.arraycopy(firstHalf, 0, res, 0, firstHalf.length);
-        
-        for (int i = mid; i < arr.length; i++) {
-            res[i] = secondHalf[i - mid];
-        }
+        int[] res=new int[arr.length];
+        System.arraycopy(firstHalf, 0, res, 0, mid);
+        System.arraycopy(secHalf, 0, res, mid, secHalf.length);
 
         return res;
     }
