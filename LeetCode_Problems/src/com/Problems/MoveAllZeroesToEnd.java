05bba387457b29bfb008b5cc5b30e0594d6dc49d
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-04-18 15:36:17 +0530

    Create MoveAllZeroesToEnd.java

---
+++ LeetCode_Problems/src/com/Problems/MoveAllZeroesToEnd.java
@@ -0,0 +1,42 @@
+package com.Problems;
+
+import java.util.Arrays;
+
+public class MoveAllZeroesToEnd {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 0, 9, 8, 0, 6, 0, 0, 4 };
+		System.out.println("Original array : " + Arrays.toString(arr));
+		int[] res = moveZeroes(arr);
+		System.out.println("After moving all zeroes to the end : " + Arrays.toString(res));
+
+	}
+
+	private static int[] moveZeroes(int[] arr) {
+		int[] res = new int[arr.length];
+		int j = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != 0)
+				res[j++] = arr[i];
+		}
+		return res;
+	}
+
+}
+
+
+
+/**************** Output ******************/
+
+
+/*
+
+
+
+Original array : [1, 0, 9, 8, 0, 6, 0, 0, 4]
+After moving all zeroes to the end : [1, 9, 8, 6, 4, 0, 0, 0, 0]
+
+
+
+
+*/
