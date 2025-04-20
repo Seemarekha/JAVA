3c470acbb05d2574a03d834c78a8ced7b4469c5b
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-04-20 07:11:17 +0530

    Create SubArrayWithGivenSum.java

---
+++ LeetCode_Problems/src/com/Problems/SubArrayWithGivenSum.java
@@ -0,0 +1,26 @@
+package com.Problems;
+
+public class SubArrayWithGivenSum {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 7, 5 };
+		findSubarray(arr, 15);
+	}
+
+	private static void findSubarray(int[] arr, int sum) {
+		int i = 0, currSum = 0;
+		for (int j = 0; j < arr.length; j++) {
+			currSum += arr[j];
+			while (currSum > sum && i < j) {
+				currSum -= arr[i++];
+			}
+			if (currSum == sum) {
+				System.out.println("Subarray from index " + i + " to " + j);
+				return;
+			}
+		}
+		System.out.println("Not found");
+
+	}
+
+}
