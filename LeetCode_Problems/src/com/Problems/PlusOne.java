2cd4b454fb14f7382afca40b3327acb3bbe64111
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-06-25 19:32:49 +0530

    Create PlusOne.java

---
+++ LeetCode_Problems/src/com/Problems/PlusOne.java
@@ -0,0 +1,31 @@
+package com.Problems;
+
+import java.util.Arrays;
+
+public class PlusOne {
+
+	public static void main(String[] args) {
+		int[] nums= {1,9};
+		int[] res=plusOne(nums);
+		System.out.println(Arrays.toString(res));
+		
+	}
+
+	private static int[] plusOne(int[] nums) {
+		int n=nums.length;
+		for(int i=n-1;i>=0;i--)
+		{
+			if(nums[i]<9)
+			{
+				nums[i]++;
+				return nums;
+			}
+			nums[i]=0;
+		}
+		int[] result=new int[n+1];
+		result[0]=1;
+		return result;
+		
+	}
+
+}
