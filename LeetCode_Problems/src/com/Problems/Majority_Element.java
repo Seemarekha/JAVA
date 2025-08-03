ef56d4806f4e0b3ad2049cd326bf78578d316ccb
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-08-03 10:23:18 +0530

    Create Majority_Element.java

---
+++ LeetCode_Problems/src/com/Problems/Majority_Element.java
@@ -0,0 +1,45 @@
+/*
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+ */
+
+package com.Problems;
+
+public class Majority_Element {
+
+	public static void main(String[] args) {
+		int[] nums1 = { 3, 2, 3 };
+		int[] nums2 = { 2, 2, 1, 1, 1, 2, 2 };
+
+		System.out.println("Majority Element in nums1: " + majorityElement(nums1)); // Output: 3
+		System.out.println("Majority Element in nums2: " + majorityElement(nums2)); // Output: 2
+
+	}
+
+	private static int majorityElement(int[] nums) {
+		int count = 0;
+		Integer candidate = null;
+
+		for (int num : nums) {
+			if (count == 0)
+				candidate = num;
+
+			count += (num == candidate) ? 1 : -1;
+		}
+		return candidate;
+	}
+
+}
