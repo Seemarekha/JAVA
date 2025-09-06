3c322b5927f89276f35a2dd2c41b23f8bbf97d4e
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-09-06 22:17:01 +0530

    Create Longest_Valid_Parentheses.java

---
+++ LeetCode_Problems/src/com/Problems/Longest_Valid_Parentheses.java
@@ -0,0 +1,60 @@
+/*
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+
+
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+
+
+Example 3:
+
+Input: s = ""
+Output: 0
+ */
+
+package com.Problems;
+
+import java.util.Stack;
+
+public class Longest_Valid_Parentheses {
+
+	public static void main(String[] args) {
+		System.out.println(longestValidParentheses("(()")); // 2
+		System.out.println(longestValidParentheses(")()())")); // 4
+		System.out.println(longestValidParentheses("")); // 0
+
+	}
+
+	private static int longestValidParentheses(String s) {
+		int maxLen = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(-1);
+
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+
+			if (c == '(') {
+				stack.push(i);
+			} else {
+				stack.pop();
+				if (stack.isEmpty()) {
+					stack.push(i);
+				} else {
+					maxLen = Math.max(maxLen, i - stack.peek());
+				}
+			}
+		}
+
+		return maxLen;
+	}
+
+}
