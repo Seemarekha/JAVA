22c14e8af457a13781a17f643e51b7bd2e80a928
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-09-11 10:41:22 +0530

    Update Maximum_SubArray.java

--- LeetCode_Problems/src/com/Problems/Maximum_SubArray.java
+++ LeetCode_Problems/src/com/Problems/Maximum_SubArray.java
@@ -1,48 +1,53 @@
-package com.Problems;
+/*
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
 
-import java.util.Arrays;
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+
+
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+ */
+
+package com.Problems;
 
 public class Maximum_SubArray {
 
 	public static void main(String[] args) {
-		int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-		System.out.println("Given Array : " + Arrays.toString(arr));
-		int maxValue = maxSubArray(arr);
-		System.out.println("Sum of maximum subArray : " + maxValue);
+		int[] nums1 = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		System.out.println(maxSubArray(nums1)); // 6
+
+		int[] nums2 = { 1 };
+		System.out.println(maxSubArray(nums2)); // 1
+
+		int[] nums3 = { 5, 4, -1, 7, 8 };
+		System.out.println(maxSubArray(nums3)); // 23
 
 	}
 
 	private static int maxSubArray(int[] arr) {
-		int currSum = 0;
+		int currSum = arr[0];
 		int maxSum = arr[0];
 
-		for (int i = 0; i < arr.length; i++) {
-			if (currSum < 0)
-				currSum = 0;
-			currSum = currSum + arr[i];
-			maxSum = Math.max(maxSum, currSum);
+		for (int i = 1; i < arr.length; i++) {
+			currSum = Math.max(arr[i], currSum + arr[i]);
+			maxSum = Math.max(currSum, maxSum);
 		}
 		return maxSum;
 	}
 
 }
-
-
-
-
-
-
-/******************** Output *********************/
-
-
-/*
- 
- 
- 
-Given Array : [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-Sum of maximum subArray : 6 
- 
- 
- 
- 
- */
