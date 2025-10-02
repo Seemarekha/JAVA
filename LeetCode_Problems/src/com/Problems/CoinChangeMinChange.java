207ec619d694b5599878e6b9415e368a6cc707d0
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-02 10:18:16 +0530

    Update CoinChangeMinChange.java

--- LeetCode_Problems/src/com/Problems/CoinChangeMinChange.java
+++ LeetCode_Problems/src/com/Problems/CoinChangeMinChange.java
@@ -4,7 +4,6 @@ You are given an integer array coins representing coins of different denominatio
 Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 
 You may assume that you have an infinite number of each kind of coin.
-
  
 
 Example 1:
@@ -12,10 +11,14 @@ Example 1:
 Input: coins = [1,2,5], amount = 11
 Output: 3
 Explanation: 11 = 5 + 5 + 1
+
+
 Example 2:
 
 Input: coins = [2], amount = 3
 Output: -1
+
+
 Example 3:
 
 Input: coins = [1], amount = 0
