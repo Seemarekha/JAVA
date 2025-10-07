a0b7c864665fcf042643d5986c29633c86286666
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-07 16:21:05 +0530

    Update PrintOddeIndexValue.java

--- Array_Programs/src/com/array_programs/PrintOddeIndexValue.java
+++ Array_Programs/src/com/array_programs/PrintOddeIndexValue.java
@@ -5,20 +5,18 @@ import java.util.Arrays;
 public class PrintOddeIndexValue {
 
 	public static void main(String[] args) {
-		int[] arr = {10, 20, 30, 40, 50, 60, 70};
-		System.out.println("Array values :");
-		System.out.println(Arrays.toString(arr));
+		int[] arr = { 10, 20, 30, 40, 50, 60, 70 };
+		System.out.println("Given Array : " + Arrays.toString(arr));
 		System.out.println("Values at odd indices:");
-		 int[] res=new int[arr.length/2];
-		 int j=0;
-		 for(int i=0;i<arr.length;i++) {
-			 if(i%2!=0)
-			 {
-				res[j++]+=arr[i];
-			 }
-		 }
+		int[] res = new int[arr.length / 2];
+		int j = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (i % 2 != 0) {
+				res[j++] += arr[i];
+			}
+		}
 
-		 System.out.println(Arrays.toString(res));
+		System.out.println(Arrays.toString(res));
 
 	}
 
