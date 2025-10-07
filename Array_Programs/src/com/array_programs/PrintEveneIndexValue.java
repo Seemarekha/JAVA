3ea1b5c5be89ee0bc3041ec600a3f197d09dfba1
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-07 16:21:01 +0530

    Update PrintEveneIndexValue.java

--- Array_Programs/src/com/array_programs/PrintEveneIndexValue.java
+++ Array_Programs/src/com/array_programs/PrintEveneIndexValue.java
@@ -5,21 +5,19 @@ import java.util.Arrays;
 public class PrintEveneIndexValue {
 
 	public static void main(String[] args) {
-		
-		int[] arr = {10, 20, 30, 40, 50, 60, 70};
-		System.out.println("Array values :");
-		System.out.println(Arrays.toString(arr));
-		System.out.println("Values at even indices:");
-		 int[] res=new int[arr.length/2+1];
-		 int j=0;
-		 for(int i=0;i<arr.length;i++) {
-			 if(i%2==0)
-			 {
-				res[j++]+=arr[i];
-			 }
-		 }
 
-		 System.out.println(Arrays.toString(res));
+		int[] arr = { 10, 20, 30, 40, 50, 60, 70 };
+		System.out.println("Given Array :" + Arrays.toString(arr)); // [10, 20, 30, 40, 50, 60, 70]
+		System.out.print("Values at even indices:");
+		int[] res = new int[arr.length / 2 + 1];
+		int j = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (i % 2 == 0) {
+				res[j++] += arr[i];
+			}
+		}
+
+		System.out.print(Arrays.toString(res)); // [10, 30, 50, 70]
 	}
 
 }
