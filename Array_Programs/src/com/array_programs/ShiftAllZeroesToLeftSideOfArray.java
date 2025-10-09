ccf4cd67165174f96027117c3ebdc28770ff71e3
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-09 10:40:03 +0530

    Update ShiftAllZeroesToLeftSideOfArray.java

--- Array_Programs/src/com/array_programs/ShiftAllZeroesToLeftSideOfArray.java
+++ Array_Programs/src/com/array_programs/ShiftAllZeroesToLeftSideOfArray.java
@@ -6,7 +6,7 @@ public class ShiftAllZeroesToLeftSideOfArray {
 
 	public static void main(String[] args) {
 		int[] a = { 1, 0, 4, 3, 0, 6, 0, 0, 9, 8 };
-		System.out.println("Before shifting : " + Arrays.toString(a));
+		System.out.println("Before shifting : " + Arrays.toString(a)); // [1, 0, 4, 3, 0, 6, 0, 0, 9, 8]
 		int[] b = new int[a.length];
 
 		int j = a.length - 1;
@@ -17,7 +17,7 @@ public class ShiftAllZeroesToLeftSideOfArray {
 				j--;
 			}
 		}
-		System.out.println("After shifting : " + Arrays.toString(b));
+		System.out.println("After shifting : " + Arrays.toString(b)); // [0, 0, 0, 0, 8, 9, 6, 3, 4, 1]
 
 	}
 
