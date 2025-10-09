ea941f2c422dde60878a6cca1dd0a95bcbf5d540
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-09 10:40:24 +0530

    Update SortTheElementsUsingSortMethod.java

--- Array_Programs/src/com/array_programs/SortTheElementsUsingSortMethod.java
+++ Array_Programs/src/com/array_programs/SortTheElementsUsingSortMethod.java
@@ -6,9 +6,9 @@ public class SortTheElementsUsingSortMethod {
 
 	public static void main(String[] args) {
 		int[] a = { 34, 23, 56, 46, 85, 90, 87, 54 };
-		System.out.println("Before sorting : " + Arrays.toString(a));
+		System.out.println("Before sorting : " + Arrays.toString(a)); // [34, 23, 56, 46, 85, 90, 87, 54]
 		Arrays.sort(a);
-		System.out.println("After sorting : " + Arrays.toString(a));
+		System.out.println("After sorting : " + Arrays.toString(a)); // [23, 34, 46, 54, 56, 85, 87, 90]
 
 	}
 
