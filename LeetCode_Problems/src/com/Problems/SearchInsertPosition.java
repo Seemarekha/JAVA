27463a4a285c60df67cb8093179ad460ef502294
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-14 10:12:03 +0530

    Update SearchInsertPosition.java

--- LeetCode_Problems/src/com/Problems/SearchInsertPosition.java
+++ LeetCode_Problems/src/com/Problems/SearchInsertPosition.java
@@ -1,29 +1,26 @@
 package com.Problems;
 
-
 public class SearchInsertPosition {
 
 	public static void main(String[] args) {
-		int[] arr= {1,3,5,6};
-		int target=2;
-		int res=searchInsert(arr,target);
-		System.out.println(res);
+		int[] arr = { 1, 3, 5, 6 };
+		int target = 2;
+		int res = searchInsert(arr, target);
+		System.out.println(res); // 1
 
 	}
 
-	private static int searchInsert(int[] arr,int target) {
-		int left=0,right=arr.length-1;
-		
-		
-		while(left<=right)
-		{
-			int mid=left+(right-left)/2;
-			if(target==arr[mid])
+	private static int searchInsert(int[] arr, int target) {
+		int left = 0, right = arr.length - 1;
+
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (target == arr[mid])
 				return mid;
-			else if(target<arr[mid])
-				right=mid-1;
+			else if (target < arr[mid])
+				right = mid - 1;
 			else
-				left=mid+1;
+				left = mid + 1;
 		}
 		return left;
 	}
