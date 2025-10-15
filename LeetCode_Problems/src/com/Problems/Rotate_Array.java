dede780754d64eb4278ad2efb94e9952080c366d
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-15 14:37:51 +0530

    Update Rotate_Array.java

--- LeetCode_Problems/src/com/Problems/Rotate_Array.java
+++ LeetCode_Problems/src/com/Problems/Rotate_Array.java
@@ -57,17 +57,28 @@ public class Rotate_Array {
 	/************* Approach -2 ********************/
 
 	/*
-	 * public static void main(String[] args) { int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
-	 * for(int i=1;i<=3;i++) { rotate(arr); }
-	 * 
-	 * System.out.println(Arrays.toString(arr));
-	 * 
-	 * }
-	 * 
-	 * private static void rotate(int[] arr) { int temp=arr[arr.length-1]; for(int
-	 * j=arr.length-2;j>=0;j--) { arr[j+1]=arr[j]; } arr[0]=temp;
-	 * 
-	 * }
+	  public static void main(String[] args) 
+	  { 
+	  		int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+	  		for(int i=1;i<=3;i++) 
+	  		{ 
+	  			rotate(arr);
+	  		}
+	  
+	  		System.out.println(Arrays.toString(arr));
+	  
+	  }
+	  
+	  private static void rotate(int[] arr) 
+	  { 
+	  		int temp=arr[arr.length-1]; 
+	  		for(int j=arr.length-2;j>=0;j--) 
+	  		{ 
+	  			arr[j+1]=arr[j]; 
+	  		} 
+	  		arr[0]=temp;
+	  
+	  }
 	 */
 
 }
