9512b85d68bc07e0ffb4c8f3ffdfdd9a0473b27e
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-22 12:40:49 +0530

    Update PlusOne.java

--- LeetCode_Problems/src/com/Problems/PlusOne.java
+++ LeetCode_Problems/src/com/Problems/PlusOne.java
@@ -5,27 +5,25 @@ import java.util.Arrays;
 public class PlusOne {
 
 	public static void main(String[] args) {
-		int[] nums= {1,9};
-		int[] res=plusOne(nums);
-		System.out.println(Arrays.toString(res));
-		
+		int[] nums = { 1, 9, 3, 9 };
+		int[] res = plusOne(nums);
+		System.out.println(Arrays.toString(res)); // [1, 9, 4, 0]
+
 	}
 
 	private static int[] plusOne(int[] nums) {
-		int n=nums.length;
-		for(int i=n-1;i>=0;i--)
-		{
-			if(nums[i]<9)
-			{
+		int n = nums.length;
+		for (int i = n - 1; i >= 0; i--) {
+			if (nums[i] < 9) {
 				nums[i]++;
 				return nums;
 			}
-			nums[i]=0;
+			nums[i] = 0;
 		}
-		int[] result=new int[n+1];
-		result[0]=1;
+		int[] result = new int[n + 1];
+		result[0] = 1;
 		return result;
-		
+
 	}
 
 }
