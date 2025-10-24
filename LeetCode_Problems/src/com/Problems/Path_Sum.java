20a52f6ce70f0f72ce6b052d6a463f138676ff0d
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-24 09:34:09 +0530

    Update Path_Sum.java

--- LeetCode_Problems/src/com/Problems/Path_Sum.java
+++ LeetCode_Problems/src/com/Problems/Path_Sum.java
@@ -10,6 +10,8 @@ Example 1:
 Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
 Output: true
 Explanation: The root-to-leaf path with the target sum is shown.
+
+
 Example 2:
 
 
@@ -19,6 +21,8 @@ Explanation: There are two root-to-leaf paths in the tree:
 (1 --> 2): The sum is 3.
 (1 --> 3): The sum is 4.
 There is no root-to-leaf path with sum = 5.
+
+
 Example 3:
 
 Input: root = [], targetSum = 0
@@ -69,3 +73,5 @@ public class Path_Sum {
 	}
 
 }
+
+
