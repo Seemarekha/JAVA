360fa0e71b1cba6da1d7f98d8d3caad9e14e09a0
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-26 10:20:18 +0530

    Update Minimum_Absolute_Difference_In_BST.java

--- LeetCode_Problems/src/com/Problems/Minimum_Absolute_Difference_In_BST.java
+++ LeetCode_Problems/src/com/Problems/Minimum_Absolute_Difference_In_BST.java
@@ -3,11 +3,11 @@ Given the root of a Binary Search Tree (BST), return the minimum absolute differ
 
 Example 1:
 
-
 Input: root = [4,2,6,1,3]
 Output: 1
-Example 2:
 
+
+Example 2:
 
 Input: root = [1,0,48,null,null,12,49]
 Output: 1
