4f6ca094d04b6df3c551a710a30fea8f8c816483
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-10-27 10:34:48 +0530

    Update Number_Complement.java

--- LeetCode_Problems/src/com/Problems/Number_Complement.java
+++ LeetCode_Problems/src/com/Problems/Number_Complement.java
@@ -4,13 +4,14 @@ The complement of an integer is the integer you get when you flip all the 0's to
 For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
 Given an integer num, return its complement.
 
- 
 
 Example 1:
 
 Input: num = 5
 Output: 2
 Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+
+
 Example 2:
 
 Input: num = 1
