6ceb5716475b7c4be0339aa4e6c58bec379bb14c
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-12-06 10:15:40 +0530

    Update Jewels_And_Stones.java

--- LeetCode_Problems/src/com/Problems/Jewels_And_Stones.java
+++ LeetCode_Problems/src/com/Problems/Jewels_And_Stones.java
@@ -1,5 +1,5 @@
 /*
- You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
 
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
@@ -8,6 +8,8 @@ Example 1:
 
 Input: jewels = "aA", stones = "aAAbbbb"
 Output: 3
+
+
 Example 2:
 
 Input: jewels = "z", stones = "ZZ"
