eab66177f3830a91aa91e383c98542f3baf9b87a
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-12-10 09:21:51 +0530

    Update LongestUncommonSubsequence.java

--- LeetCode_Problems/src/com/Problems/LongestUncommonSubsequence.java
+++ LeetCode_Problems/src/com/Problems/LongestUncommonSubsequence.java
@@ -10,11 +10,15 @@ Input: a = "aba", b = "cdc"
 Output: 3
 Explanation: One longest uncommon subsequence is "aba" because "aba" is a subsequence of "aba" but not "cdc".
 Note that "cdc" is also a longest uncommon subsequence.
+
+
 Example 2:
 
 Input: a = "aaa", b = "bbb"
 Output: 3
 Explanation: The longest uncommon subsequences are "aaa" and "bbb".
+
+
 Example 3:
 
 Input: a = "aaa", b = "aaa"
