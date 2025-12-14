ceb2d62f1e0f905fdace539d3e53da3886123346
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-12-14 09:39:14 +0530

    Update Palindrome_Check.java

--- LeetCode_Problems/src/com/Problems/Palindrome_Check.java
+++ LeetCode_Problems/src/com/Problems/Palindrome_Check.java
@@ -9,7 +9,7 @@ public class Palindrome_Check {
 	}
 
 	private static boolean isPalindrome(String str) {
-		str = str.replaceAll("[a-zA-Z0-9]", "");
+		str = str.toLowerCase().replaceAll(" ", "");
 		int i = 0, j = str.length() - 1;
 		while (i < j) {
 			if (str.charAt(i) != str.charAt(j))
