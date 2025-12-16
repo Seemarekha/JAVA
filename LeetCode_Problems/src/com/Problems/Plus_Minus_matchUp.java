8fea9f88271d8e10d1024b3f926fc032f0711ad8
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-12-16 09:07:45 +0530

    Update Plus_Minus_matchUp.java

--- LeetCode_Problems/src/com/Problems/Plus_Minus_matchUp.java
+++ LeetCode_Problems/src/com/Problems/Plus_Minus_matchUp.java
@@ -12,7 +12,9 @@ Aj: an integer, the number on the first button of the controller
 Bj: an integer, the number on the second button of the controller
 For each test case, your function should return a boolean value: YES if the game is winnable using the controller, and NO otherwise.
 
+
 Input Format
+
 The first line contains a single integer N  — the number of rounds.
 
 The second line contains a string s of length n— where si is the symbol that will appear on the screen in the i-th round. It is guaranteed that s contains only the characters + and -.
@@ -21,17 +23,23 @@ The third line contains an integer q (1≤q≤1e5) — the number of controllers
 
 The following q lines contain two space-separated integers Aj and Bj each (1≤Aj,Bj≤1e9) — the numbers on the buttons of controller j.
 
+
 Output Format
+
 Output q lines. On line j print YES if the game is winnable using controller j, otherwise print NO
 
+
 Constraints
+
 1<=N<=2*10^5
 
 1<=q<=10^5
 
 1<=Ai,Bi<=10^9
 
+
 Sample Testcase 0
+
 Testcase Input
 8
 +-+---+-
@@ -41,28 +49,35 @@ Testcase Input
 7 9
 10 10
 5 3
+
 Testcase Output
 YES
 NO
 NO
 NO
 YES
+
 Explanation
 For the first round there is a possible sequence on which we can get zero, one possible way to get score 0 using the first controller is by pressing the button with number 1  in rounds 1, 2, 4, 5, 6 and 8, and pressing the button with number 2 in rounds 3 and 7.
 For the second round there is no possible sequence so that answer is zero.
 For the third round there is no possible sequence so that answer is zero.
 For the fourth round there is no possible sequence so that answer is zero.
 For the fifth test case there is a possible solution.
+
+
 Sample Testcase 1
+
 Testcase Input
 20
 +-----+--+--------+-
 2
 1000000000 99999997
 250000000 1000000000
+
 Testcase Output
 NO
 YES
+
 Explanation
 For the first round there is no possible combination so the answer is No.
 For the second round there is possible combination so the answer is Yes
