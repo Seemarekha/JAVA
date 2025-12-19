dcc9616968bff62ab623e7003d9761777125de6d
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-12-19 09:22:20 +0530

    Update Repeated_Substring_Pattern.java

--- LeetCode_Problems/src/com/Problems/Repeated_Substring_Pattern.java
+++ LeetCode_Problems/src/com/Problems/Repeated_Substring_Pattern.java
@@ -6,10 +6,14 @@ Example 1:
 Input: s = "abab"
 Output: true
 Explanation: It is the substring "ab" twice.
+
+
 Example 2:
 
 Input: s = "aba"
 Output: false
+
+
 Example 3:
 
 Input: s = "abcabcabcabc"
