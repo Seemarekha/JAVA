3ed920b6fb8acc4680ca18960e6fd6ae08f422f2
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-12-21 09:59:05 +0530

    Update Robot_Return_To_Origin.java

--- LeetCode_Problems/src/com/Problems/Robot_Return_To_Origin.java
+++ LeetCode_Problems/src/com/Problems/Robot_Return_To_Origin.java
@@ -1,5 +1,5 @@
 /*
- There is a robot starting at the position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
+There is a robot starting at the position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
 
 You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move. Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
 
@@ -13,6 +13,8 @@ Example 1:
 Input: moves = "UD"
 Output: true
 Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
+
+
 Example 2:
 
 Input: moves = "LL"
