e7e352d1deaeff084313dee6d4625e7760d6bb1c
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-12-25 09:57:00 +0530

    Create InOrder_Traversal.java

---
+++ LeetCode_Problems/src/com/Problems/InOrder_Traversal.java
@@ -0,0 +1,37 @@
+package com.Problems;
+
+public class InOrder_Traversal {
+
+	static class Node {
+		int data;
+		Node left, right;
+
+		Node(int data) {
+			this.data = data;
+			left = right = null;
+		}
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+
+		System.out.print("InOrder Traversal: ");
+		inorder(root); // InOrder Traversal: 4 2 5 1 3 
+
+	}
+
+	private static void inorder(Node root) {
+		if (root == null)
+			return;
+
+		inorder(root.left);
+		System.out.print(root.data + " ");
+		inorder(root.right);
+
+	}
+
+}
