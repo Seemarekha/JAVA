38bf0b2808ccd2a96cc58b8b7122ea6021b92640
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-01-03 09:48:53 +0530

    Update WordOfStringInReverse.java

--- String_Programs/src/com/string_programs/WordOfStringInReverse.java
+++ String_Programs/src/com/string_programs/WordOfStringInReverse.java
@@ -5,34 +5,30 @@ import java.util.Scanner;
 public class WordOfStringInReverse {
 
 	public static void main(String[] args) {
-		Scanner scn=new Scanner(System.in);
+		Scanner scn = new Scanner(System.in);
 		System.out.println("Enter a string : ");
-		String s=scn.nextLine();
-		String[] str=s.split(" ");
-		String s2="";
-		
-		for(int i=0;i<str.length;i++)
-		{
-			String word=str[i];
-			String revWord="";
-			for(int j=word.length()-1;j>=0;j--)
-			{
-				revWord+=word.charAt(j);
+		String s = scn.nextLine();
+		String[] str = s.split(" ");
+		String s2 = "";
+
+		for (int i = 0; i < str.length; i++) {
+			String word = str[i];
+			String revWord = "";
+			for (int j = word.length() - 1; j >= 0; j--) {
+				revWord += word.charAt(j);
 			}
-			s2+=revWord;
-			if(i<str.length-1)
-				s2+=" ";
+			s2 += revWord;
+			if (i < str.length - 1)
+				s2 += " ";
 		}
 
-		System.out.println("Reverser string : "+s2);
+		System.out.println("Reverser string : " + s2);
 	}
 
 }
 
-
 /*
-
-i/p : java is very easy
-o/p : avaj si yrev ysae
-
-*/
+ * 
+ * i/p : java is very easy o/p : avaj si yrev ysae
+ * 
+ */
