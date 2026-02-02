e3b88acb4813343ec1fc7ae7389c47c6e862954e
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-02-02 10:49:17 +0530

    Update Longest_Substring_Without_Repeating_Characters.java

--- LeetCode_Problems/src/com/Problems/Longest_Substring_Without_Repeating_Characters.java
+++ LeetCode_Problems/src/com/Problems/Longest_Substring_Without_Repeating_Characters.java
@@ -8,7 +8,6 @@ public class Longest_Substring_Without_Repeating_Characters {
 	public static void main(String[] args) {
 		System.out.print("Enter a string : ");
 		String s = new Scanner(System.in).next();
-		System.out.println("Given String : " + s);
 		int res = lengthOfLongestSubstring(s);
 		System.out.println("Length of longest substring without repeating characters : " + res);
 
@@ -41,7 +40,6 @@ public class Longest_Substring_Without_Repeating_Characters {
 
 
 Enter a string : abcabcbb
-Given String : abcabcbb
 Length of longest substring without repeating characters : 3
 
 
