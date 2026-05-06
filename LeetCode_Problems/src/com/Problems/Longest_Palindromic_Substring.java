a7140378c066e162559daf998e634682f001edcc
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-05-06 09:49:34 +0530

    Update Longest_Palindromic_Substring.java

--- LeetCode_Problems/src/com/Problems/Longest_Palindromic_Substring.java
+++ LeetCode_Problems/src/com/Problems/Longest_Palindromic_Substring.java
@@ -3,7 +3,7 @@ package com.Problems;
 public class Longest_Palindromic_Substring {
 
 	public static void main(String[] args) {
-		String s = "ba bd";
+		String s = "babd";
 		System.out.println(longestPalindrome(s));  // bab
 	}
 
