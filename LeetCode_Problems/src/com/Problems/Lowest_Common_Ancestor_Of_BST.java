647989d1d6126af72b595cf16015bf09f0832561
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-05-06 09:49:38 +0530

    Update Lowest_Common_Ancestor_Of_BST.java

--- LeetCode_Problems/src/com/Problems/Lowest_Common_Ancestor_Of_BST.java
+++ LeetCode_Problems/src/com/Problems/Lowest_Common_Ancestor_Of_BST.java
@@ -6,6 +6,7 @@ According to the definition of LCA on Wikipedia: “The lowest common ancestor i
 
 Example 1:
 
+
 Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
 Output: 6
 Explanation: The LCA of nodes 2 and 8 is 6.
