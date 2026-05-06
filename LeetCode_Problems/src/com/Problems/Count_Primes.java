fd849ee1dd2e6cc07d397ee5ea36a06668a8735a
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-05-06 09:49:25 +0530

    Update Count_Primes.java

--- LeetCode_Problems/src/com/Problems/Count_Primes.java
+++ LeetCode_Problems/src/com/Problems/Count_Primes.java
@@ -23,7 +23,6 @@ Output: 0
 
 package com.Problems;
 
-import java.util.Arrays;
 
 public class Count_Primes {
 
@@ -32,8 +31,8 @@ public class Count_Primes {
 		int n = 10;
 		System.out.println("Number of primes less than " + n + " = " + countPrimes(n)); // 4
 
-		n = 0;
-		System.out.println("Number of primes less than " + n + " = " + countPrimes(n)); // 0
+		n = 15;
+		System.out.println("Number of primes less than " + n + " = " + countPrimes(n)); // 6
 
 		n = 1;
 		System.out.println("Number of primes less than " + n + " = " + countPrimes(n)); // 0
@@ -41,28 +40,45 @@ public class Count_Primes {
 	}
 
 	private static int countPrimes(int n) {
-		if (n <= 2)
-			return 0;
-
-		boolean[] isPrime = new boolean[n];
-		Arrays.fill(isPrime, true);
-		isPrime[0] = isPrime[1] = false;
-
-		for (int i = 2; i * i < n; i++) {
-			if (isPrime[i]) {
-				for (int j = i * i; j < n; j += i) {
-					isPrime[j] = false;
-				}
-			}
-		}
-
-		int count = 0;
-		for (boolean prime : isPrime) {
-			if (prime)
+//		if (n <= 2)
+//			return 0;
+//
+//		boolean[] isPrime = new boolean[n];
+//		Arrays.fill(isPrime, true);
+//		isPrime[0] = isPrime[1] = false;
+//
+//		for (int i = 2; i * i < n; i++) {
+//			if (isPrime[i]) {
+//				for (int j = i * i; j < n; j += i) {
+//					isPrime[j] = false;
+//				}
+//			}
+//		}
+//
+//		int count = 0;
+//		for (boolean prime : isPrime) {
+//			if (prime)
+//				count++;
+//		}
+//
+//		return count;
+		
+		if(n<2) return 0;
+		int count=0;
+		for(int i=2;i<n;i++)
+		{
+			if(isPrime(i,i/2))
 				count++;
 		}
-
 		return count;
+	}
+
+	private static boolean isPrime(int n,int i) {
+		if(n<=1) return false;
+		if(i==1)
+			return true;
+		if(n%i==0) return false;
+		return isPrime(n,i-1);
 	}
 
 }
