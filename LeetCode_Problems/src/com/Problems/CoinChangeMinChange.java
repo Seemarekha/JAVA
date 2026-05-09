a114f7b2664315342497e1ec4eee5d9df43a56bd
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-05-09 09:39:04 +0530

    Update CoinChangeMinChange.java

--- LeetCode_Problems/src/com/Problems/CoinChangeMinChange.java
+++ LeetCode_Problems/src/com/Problems/CoinChangeMinChange.java
@@ -45,13 +45,15 @@ public class CoinChangeMinChange {
 
 		dp[0] = 0;
 
-		for (int coin : coins) {
-			for (int i = coin; i <= amount; i++) {
-				dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+		for(int i=1;i<=amount;i++)
+		{
+			for(int coin:coins)
+			{
+				if(coin<=i)
+					dp[i]=Math.min(dp[i],dp[i-coin]+1);
 			}
 		}
-
-		return dp[amount] > amount ? -1 : dp[amount];
+		return dp[amount];
 	}
 
 }
