7286cd413bc612b747b73d6684f9def26d662089
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-05-09 09:39:08 +0530

    Update Climbing_Stairs.java

--- LeetCode_Problems/src/com/Problems/Climbing_Stairs.java
+++ LeetCode_Problems/src/com/Problems/Climbing_Stairs.java
@@ -6,48 +6,62 @@ public class Climbing_Stairs {
 
 	public static void main(String[] args) {
 		System.out.print("Enter number of steps : ");
-		int n=new Scanner(System.in).nextInt();
-		System.out.println("Number of ways to climbs "+n+" steps :"+climbStairs(n));
+		int n = new Scanner(System.in).nextInt();
+		System.out.println("Number of ways to climbs " + n + " steps :" + climbStairs(n));
 
 	}
+
+	// using dp
 
 	private static int climbStairs(int n) {
-		if(n<=1) return 1;
-		
-		int first=1;
-		int second=1;
-		int total=0;
-		
-		for(int i=2;i<=n;i++)
-		{
-			total=first+second;
-			first=second;
-			second=total;
+		int[] dp = new int[n + 1];
+
+		dp[0] = 1;
+		dp[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
 		}
-		return second;
+		return dp[n];
 	}
 
-}
+	// Normal approach
+//	private static int climbStairs(int n) {
+//		if(n<=1) return 1;
+//		
+//		int first=1;
+//		int second=1;
+//		int total=0;
+//		
+//		for(int i=2;i<=n;i++)
+//		{
+//			total=first+second;
+//			first=second;
+//			second=total;
+//		}
+//		return second;
+//	}
 
+}
 
 /***************** Output *******************/
 
 /*
- 
- 
-Enter number of steps : 2
-Number of ways to climbs 2 steps :2
-
-
-Enter number of steps : 3
-Number of ways to climbs 3 steps :3
- 
- 
- 
-Enter number of steps : 5
-Number of ways to climbs 5 steps :8
- 
- 
- 
- 
+ * 
+ * 
+ * Enter number of steps : 2 
+ * Number of ways to climbs 2 steps :2
+ * 
+ * 
+ * Enter number of steps : 3 
+ * Number of ways to climbs 3 steps :3
+ * 
+ * 
+ * 
+ * Enter number of steps : 5 
+ * Number of ways to climbs 5 steps :8
+ * 
+ * 
+ * 
+ * 
  */
