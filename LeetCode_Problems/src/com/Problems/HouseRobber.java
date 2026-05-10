c16f3195e537204a379b3e550b030c6403d9143b
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-05-10 10:13:47 +0530

    Update HouseRobber.java

--- LeetCode_Problems/src/com/Problems/HouseRobber.java
+++ LeetCode_Problems/src/com/Problems/HouseRobber.java
@@ -8,16 +8,34 @@ public class HouseRobber {
 
 	}
 
+	// Using dp
+	
 	private static int rob(int[] nums) {
-		int prev = 0;
-		int curr = 0;
-
-		for (int n : nums) {
-			int temp = Math.max(curr, prev + n);
-			prev = curr;
-			curr = temp;
+		int n=nums.length;
+		
+		int[] dp=new int[n];
+		dp[0]=nums[0];
+		dp[1]=Math.max(nums[0], nums[1]);
+		
+		for(int i=1;i<n;i++)
+		{
+			dp[i]=Math.max(dp[i-1], dp[i-2]+nums[i]);
 		}
-		return curr;
+		return dp[n-1];
 	}
+
+	// Normal approach
+	
+//	private static int rob(int[] nums) {
+//		int prev = 0;
+//		int curr = 0;
+//
+//		for (int n : nums) {
+//			int temp = Math.max(curr, prev + n);
+//			prev = curr;
+//			curr = temp;
+//		}
+//		return curr;
+//	}
 
 }
