b4028cd7f9dbc393d0787100331e77ae3a4a56b5
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-05-13 10:12:59 +0530

    Create Spiral_Matrix_II.java

---
+++ LeetCode_Problems/src/com/Problems/Spiral_Matrix_II.java
@@ -0,0 +1,82 @@
+/*
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+
+
+Example 1:
+
+
+Input: n = 3
+Output: [[1,2,3],[8,9,4],[7,6,5]]
+
+
+Example 2:
+
+Input: n = 1
+Output: [[1]]
+ */
+
+package com.Problems;
+
+import java.util.Arrays;
+
+public class Spiral_Matrix_II {
+
+	public static void main(String[] args) {
+		int n = 3;
+		int[][] result = generateMatrix(n);
+
+		for (int[] row : result) {
+			System.out.println(Arrays.toString(row));
+		}
+
+	}
+
+	private static int[][] generateMatrix(int n) {
+		int[][] matrix = new int[n][n];
+
+		int left = 0, right = n - 1;
+		int top = 0, bottom = n - 1;
+
+		int num = 1;
+
+		while (top <= bottom && left <= right) {
+			for (int i = left; i <= right; i++) {
+				matrix[top][i] = num++;
+			}
+			top++;
+
+			for (int i = top; i <= bottom; i++) {
+				matrix[i][right] = num++;
+			}
+			right--;
+
+			if (top <= bottom) {
+				for (int i = right; i >= left; i--) {
+					matrix[bottom][i] = num++;
+				}
+				bottom--;
+			}
+
+			if (left <= right) {
+				for (int i = bottom; i >= top; i--) {
+					matrix[i][left] = num++;
+				}
+				left++;
+			}
+		}
+		return matrix;
+	}
+
+}
+
+
+/***************** Output ***************/
+
+/*
+
+[1, 2, 3]
+[8, 9, 4]
+[7, 6, 5]
+
+
+*/
