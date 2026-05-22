53e818d5062a2334a438e8881af214684d316997
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-05-22 09:51:36 +0530

    Update RemoveDigitMaximize.java

--- Infosys_PYQ/src/com/Infosys_PYQ/RemoveDigitMaximize.java
+++ Infosys_PYQ/src/com/Infosys_PYQ/RemoveDigitMaximize.java
@@ -1,8 +1,9 @@
 /*
 You are given a string "number" representing a positive integer and a character digit. 
+
 Return the resulting string after removing exactly one occurrence of digit from number such that the value of the 
-resulting string in decimal form is maximized. The test cases are generated such that digit occurs at least once in the 
-number. 
+resulting string in decimal form is maximized. 
+The test cases are generated such that digit occurs at least once in the number. 
 
 i/p : number="1321"
       digit d='1'
