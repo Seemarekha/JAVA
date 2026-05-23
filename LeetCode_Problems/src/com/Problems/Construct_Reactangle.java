5a16fe7f7f005434d870335789e3a30cfaf9c892
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-05-23 09:57:36 +0530

    Update Construct_Reactangle.java

--- LeetCode_Problems/src/com/Problems/Construct_Reactangle.java
+++ LeetCode_Problems/src/com/Problems/Construct_Reactangle.java
@@ -1,5 +1,6 @@
 /*
-A web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+A web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is 
+to design a rectangular web page, whose length L and width W satisfy the following requirements:
 
 The area of the rectangular web page you designed must equal to the given target area.
 The width W should not be larger than the length L, which means L >= W.
@@ -11,7 +12,8 @@ Example 1:
 Input: area = 4
 Output: [2,2]
 Explanation: The target area is 4, and all the possible ways to construct it are [1,4], [2,2], [4,1]. 
-But according to requirement 2, [1,4] is illegal; according to requirement 3,  [4,1] is not optimal compared to [2,2]. So the length L is 2, and the width W is 2.
+But according to requirement 2, [1,4] is illegal; according to requirement 3,  [4,1] is not optimal compared to [2,2]. So the 
+length L is 2, and the width W is 2.
 
 
 Example 2:
