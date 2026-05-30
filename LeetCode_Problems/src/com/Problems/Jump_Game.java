a9e9a8299a434bbc8bb66aaa9d135367550df41c
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2026-05-30 10:17:05 +0530

    Update Jump_Game.java

--- LeetCode_Problems/src/com/Problems/Jump_Game.java
+++ LeetCode_Problems/src/com/Problems/Jump_Game.java
@@ -1,5 +1,6 @@
 /*
-You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array 
+represents your maximum jump length at that position.
 
 Return true if you can reach the last index, or false otherwise.
 
@@ -16,7 +17,8 @@ Example 2:
 
 Input: nums = [3,2,1,0,4]
 Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach 
+the last index.
  */
 
 package com.Problems;
